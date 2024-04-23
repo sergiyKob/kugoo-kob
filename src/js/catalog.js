b57b59361166924a7815254280eec44bf5e04c4f
sergiyKob <sergiykob799@gmail.com>
2024-04-23 17:27:05 +0300

    add filter catalog

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -3,3 +3,4 @@ import phonesList from './modules/phones-list.js';
 import catalog from './modules/catalog.js';
 import cart from './modules/cart.js';
 import modal from './modules/modal.js';
+import productSlider from './modules/product-slider.js';
